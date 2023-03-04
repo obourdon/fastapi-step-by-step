ff636e61e45fdf90a3c558b79dcf7fbc49a268c4
Olivier Bourdon <olivier.bourdon@squarescale.com>
2023-03-04 16:49:16 +0100

    Returns structured JSON message instead of string

--- api/handlers/demo.py
+++ api/handlers/demo.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter
 
+from api.responses.detail import DetailResponse
+
 router = APIRouter(prefix="/api/v1/demo")
 
 
 # this annotation correspond to the router variable
 # defined above
-@router.get("/")
+@router.get("/", response_model=DetailResponse)
 def hello_world():
-    return "Hello from FastAPI"
+    """
+        This is the hello world endpoint
+
+    """
+    return DetailResponse(message="Hello from FastAPI")
---
+++ api/responses/detail.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+
+
+class DetailResponse(BaseModel):
+    """
+        DetailResponse represents a response associated with a detailed message
+    """
+    message: str
