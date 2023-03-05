92732ebeebe88c85843ae1a6f7d18a117c61ab7e
Olivier Bourdon <olivier.bourdon@squarescale.com>
2023-03-05 10:37:10 +0100

    Reconfigure the main doc endpoint to be at top of service URI

--- api/api.py
+++ api/api.py
@@ -4,6 +4,6 @@ from api.handlers import demo
 
 
 def create_app():
-    app = FastAPI()
+    app = FastAPI(docs_url="/")
     app.include_router(demo.router)
     return app
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from api.api import create_app
 
 def main():
     app = create_app()
-    print("Goto http://localhost:8080/docs or http://localhost:8080/redoc")
+    print("Goto http://localhost:8080/ or http://localhost:8080/redoc")
     uvicorn.run(app, host="0.0.0.0", port=8080)
 
 
